39c12896a97214825358fabebc0157c47326d1bf
TWhela <thomas.whelan@ntlworld.com>
2023-12-04 13:28:07 +0000

    first commit, moved code from desired branch

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,47 @@
+
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { SyntheticEvent, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../../app/stores/store";
+
+export default observer(function ActivityList(){
+    const {activityStore} = useStore();
+    const {deleteActivity, activitiesByDate, loading} = activityStore;
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(clickEvent: SyntheticEvent<HTMLButtonElement>, id: string): void {
+        setTarget(clickEvent.currentTarget.name);
+        deleteActivity(id);
+    }
+    
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activitiesByDate.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>{activity.city}, {activity.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                                <Button
+                                    name={activity.id}
+                                    loading={loading && target === activity.id}
+                                    onClick={(clickEvent) => handleActivityDelete(clickEvent, activity.id)} 
+                                    floated="right" 
+                                    content="Delete" 
+                                    color="grey" 
+                                />
+                                <Label basic content={activity.category} />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+})

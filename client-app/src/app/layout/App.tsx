0ca38b968b983da9fee57a124bab89df77bfed3e
TWhela <thomas.whelan@ntlworld.com>
2023-12-05 18:49:21 +0000

    set up muliple page routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,14 @@
-import { useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-const {activityStore} = useStore();
-
-  useEffect(() => {
-    //changed request with agent.ts
-    //moved to activityStore.ts
-    activityStore.loadActivities();
-  }, [activityStore] )
-
-if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
-
   return (
     <>
     <NavBar/>
     <Container style={{marginTop: '7em'}}>
-      <h2>Welcome to Reactivitie</h2>
-      <ActivityDashboard />
+      <Outlet />
     </Container>
     </>
   )
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,10 +4,18 @@ import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useEffect } from "react";
 
 export default observer(function ActivityDashboard(){
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+    activityStore.loadActivities();
+    }, [activityStore] )
+
+if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
     return (
         <Grid>

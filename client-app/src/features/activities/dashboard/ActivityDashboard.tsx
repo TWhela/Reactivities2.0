3fb18d79a68def171ab13af322504412878c3334
TWhela <thomas.whelan@ntlworld.com>
2023-12-08 23:22:07 +0000

    created mutiple pages, created routes and updated button links

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,5 @@
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
@@ -9,11 +7,11 @@ import { useEffect } from "react";
 
 export default observer(function ActivityDashboard(){
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    const {loadActivities, activityRegistry} = activityStore;
 
     useEffect(() => {
-    activityStore.loadActivities();
-    }, [activityStore] )
+        if (activityRegistry.size <= 1) loadActivities();
+    }, [loadActivities, activityRegistry.size] )
 
 if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
@@ -23,10 +21,7 @@ if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'
                 <ActivityList/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm />}
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     )

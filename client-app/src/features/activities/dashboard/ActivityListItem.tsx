04b8065ca1d58e4b7498b87a543f05f973045765
TWhela <thomas.whelan@ntlworld.com>
2023-12-10 01:52:49 +0000

    style activities page and activity page

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
+import { SyntheticEvent, useState } from "react";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityListItem({activity}: Props) {
+    const {activityStore} = useStore();
+    const {deleteActivity, loading} = activityStore;
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(clickEvent: SyntheticEvent<HTMLButtonElement>, id: string): void {
+        setTarget(clickEvent.currentTarget.name);
+        deleteActivity(id);
+    }
+
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by *redacted*</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees will go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button 
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='blue'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
+    )
+}
